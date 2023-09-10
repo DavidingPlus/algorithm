c9a5490c86a1c4444cebf511c46fd14fe4f4e0a0
DavidingPlus <2065666169@qq.com>
2023-09-10 12:24:01 +0800

    update 9.10

--- labuladong课程/数据结构/第一章/MyArrayList/MyArrayList.cpp
+++ labuladong课程/数据结构/第一章/MyArrayList/MyArrayList.cpp
@@ -1,31 +1,60 @@
 #include "MyArrayList.h"
 
-/**
- * @brief 下面是对类内函数的实现
- */
 // 需要显式模板实例化
 template class MyArrayList<int>;
 
+/**
+ * @brief 静态变量类外初始化
+ */
+template <class value_type>
+size_t MyArrayList<value_type>::INIT_CAP = 1;
+
+/**
+ * @brief 下面是对类内函数的实现
+ */
 template <class value_type>
 inline void MyArrayList<value_type>::_init() {
     capacity = INIT_CAP;
     data = new value_type[capacity];
-    size = 0;
+    _size = 0;
 }
 
 template <class value_type>
-inline void MyArrayList<value_type>::resize(int cap) {
+inline void MyArrayList<value_type>::_resize(size_t cap) {
     // 开辟一段新空间
-    value_type *newdata = new value_type[cap];
+    pointer newdata = new value_type[cap];
     // 将原来的数组值
-    memcpy(newdata, data, sizeof(value_type) * size);
+    memcpy(newdata, data, sizeof(value_type) * _size);
 
     // 更新capacity
     capacity = cap;
 
-    value_type *tmp = data;
+    pointer tmp = data;
     data = newdata;
     delete[] tmp;
+}
+
+template <class value_type>
+void MyArrayList<value_type>::_check_index(int index, std::string func_name) {
+    // insert可以是size，但是其他的接口不能用size，这里进行特殊判断
+    try {
+        bool ret = false;
+        if ((func_name == "insert") ? true : false)
+            ret = index < 0 || index > _size;
+        else
+            ret = index < 0 || index >= _size;
+
+        if (ret) {
+            char exception_str[1024] = {0};
+            sprintf(exception_str, "%s postion was out of range.", func_name.c_str());
+
+            throw std::out_of_range(exception_str);
+        }
+        return;
+    } catch (std::out_of_range &e) {
+        std::cerr << e.what() << std::endl;
+        exit(-1);
+    }
 }
 
 template <class value_type>
@@ -36,7 +65,7 @@ inline MyArrayList<value_type>::MyArrayList() {
 // 这里需要初始化，所以需要构造函数委托
 template <class value_type>
 inline MyArrayList<value_type>::MyArrayList(std::initializer_list<value_type> list) : MyArrayList<value_type>() {
-    for (auto &val : list)
+    for (constref val : list)
         push_back(val);
 }
 
@@ -52,13 +81,13 @@ inline MyArrayList<value_type> &MyArrayList<value_type>::operator=(const MyArray
         return *this;
 
     // 不是先杀掉自己，然后赋值，深拷贝
-    size = array.size;
+    _size = array._size;
     capacity = array.capacity;
 
     auto tmp = data;
 
     data = new value_type[array.capacity];
-    memcpy(data, array.data, sizeof(value_type) * size);
+    memcpy(data, array.data, sizeof(value_type) * _size);
 
     delete[] tmp;
 
@@ -71,16 +100,86 @@ inline MyArrayList<value_type>::MyArrayList(const MyArrayList<value_type> &array
 }
 
 template <class value_type>
-inline void MyArrayList<value_type>::push_back(const reference val) {
-    if (size == capacity)  // 需要进行扩容
-        resize(2 * capacity);
+void MyArrayList<value_type>::insert(int index, constref val) {
+    _check_index(index, "insert");
 
-    // 否则在末尾插入
-    data[size++] = val;
+    // 判断是否需要进行扩容
+    if (_size == capacity)
+        _resize(2 * capacity);
+    // 从最后一个(下标size-1)到index下标都往后移动
+    // 第一次插入pos这里为-1，size_t会转化为补码，就会访问非法内存，所以这里pos使用ssize_t类型并且加上pos>0的判断
+    for (ssize_t pos = _size - 1; pos > 0 && pos >= index; --pos)
+        data[pos + 1] = data[pos];
+    // 将新元素插入
+    data[index] = val;
+    ++_size;
+}
+
+template <class value_type>
+inline void MyArrayList<value_type>::push_back(constref val) {
+    insert(_size, val);
+}
+
+template <class value_type>
+value_type MyArrayList<value_type>::remove(int index) {
+    _check_index(index, "remove");
+
+    // 删除元素
+    value_type del_val = data[index];
+
+    for (int pos = index; pos < _size - 1; ++pos)
+        data[pos] = data[pos + 1];
+
+    data[_size - 1] = (value_type)0;
+
+    // 如果删除之后的大小小于capacity的四分之一，那么我们进行缩容
+    if (--_size < capacity / 4)
+        _resize(capacity / 2);
+
+    return del_val;
+}
+
+template <class value_type>
+value_type MyArrayList<value_type>::pop_back() {
+    return remove(_size - 1);
+}
+
+template <class value_type>
+value_type MyArrayList<value_type>::set(int index, constref val) {
+    _check_index(index, "set");
+
+    value_type pre_val = data[index];
+    data[index] = val;
+
+    return pre_val;
+}
+
+template <class value_type>
+value_type MyArrayList<value_type>::get(int index) {
+    _check_index(index, "get");
+
+    return data[index];
+}
+
+template <class value_type>
+typename MyArrayList<value_type>::reference MyArrayList<value_type>::operator[](int index) {
+    _check_index(index, "operator []");
+
+    return data[index];
 }
 
 template <class value_type>
 inline void MyArrayList<value_type>::tranverse(call_back call) {
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < _size; ++i)
         call(data[i]);
+}
+
+template <class value_type>
+size_t MyArrayList<value_type>::size() {
+    return _size;
+}
+
+template <class value_type>
+bool MyArrayList<value_type>::isEmpty() {
+    return 0 == _size;
 }
--- labuladong课程/数据结构/第一章/MyArrayList/test.cpp
+++ labuladong课程/数据结构/第一章/MyArrayList/test.cpp
@@ -2,15 +2,123 @@
 using namespace std;
 #include "MyArrayList.h"
 
-int main() {
-    MyArrayList<int> al{1, 2, 3, 4, 1, 34};
+template <typename value_type>
+void print_arraylist(MyArrayList<value_type>& array) {
+    array.tranverse([](value_type& val) { cout << val << ' '; });
+    cout << endl;
+}
+
+namespace Test_ArrayList {
+
+void test_constructor() {
+    MyArrayList<int> al{1, 2, 3, 4, 5, 6};
     auto al2(al);
 
-    al.tranverse([](int& val) { cout << val << ' '; });
-    cout << endl;
+    cout << "al2: " << endl;
+    print_arraylist(al2);
 
-    al2.tranverse([](int& val) { cout << ++val << ' '; });
-    cout << endl;
+    cout << "al1: " << endl;
+    print_arraylist(al);
+}
+
+void test_insert() {
+    MyArrayList<int> al{1, 2, 3, 4, 5, 6};
+    print_arraylist(al);
+
+    // 测试在末尾插入
+    al.insert(6, 57);
+    print_arraylist(al);
+
+    al.insert(0, 100);
+    print_arraylist(al);
+
+    al.insert(3, 101);
+    print_arraylist(al);
+
+    al.insert(100, -1);
+    print_arraylist(al);
+}
+
+void test_remove() {
+    MyArrayList<int> al{1, 2, 3, 4, 5, 6};
+    print_arraylist(al);
+
+    cout << al.remove(2) << endl;
+    print_arraylist(al);
+
+    cout << al.pop_back() << endl;
+    print_arraylist(al);
+
+    // 测试弹出size的位置
+    al.remove(al.size());
+}
+
+// 这个接口的测试需要capacity是共有的，但是实际上不允许，所以我注释掉了，测试成功，需要重新测试把私有改为共有即可
+// void test_resize() {
+//     // 测试一下缩容是否正确
+//     MyArrayList<int> al{1, 2, 3, 4, 5, 6};
+//     print_arraylist(al);
+//     cout << al.size() << ' ' << al.capacity << endl;
+
+//     // 全给他弹完咯
+//     for (int i = al.size(); i > 0; --i) {
+//         al.pop_back();
+//         cout << al.size() << ' ' << al.capacity << endl;
+//     }
+// }
+
+void test_set() {
+    MyArrayList<int> al{1, 2, 3, 4, 5, 6};
+    print_arraylist(al);
+
+    al.set(0, 100);
+    print_arraylist(al);
+
+    al.set(5, 97);
+    print_arraylist(al);
+
+    // 越界
+    al.set(7, 101);
+}
+
+void test_get_operator() {
+    MyArrayList<int> al{1, 2, 3, 4, 5, 6};
+    print_arraylist(al);
+
+    cout << "test get" << endl;
+
+    cout << al.get(0) << endl;  // 修改不了，因为返回的是一个右值，如果用一个变量接受就进行了一次拷贝
+    cout << al.get(5) << endl;
+    // cout << al.get(6) << endl;
+
+    cout << endl
+         << "test operator[]" << endl;
+
+    cout << al[0] << endl;
+
+    al[1] = 100;
+    print_arraylist(al);
+
+    cout << al[6] << endl;
+}
+
+}  // namespace Test_ArrayList
+
+int main() {
+    Test_ArrayList::test_constructor();
+    cout << endl
+         << "-------------------------------------" << endl
+         << endl;
+
+    // Test_ArrayList::test_insert();
+
+    // Test_ArrayList::test_remove();
+
+    // Test_ArrayList::test_resize();
+
+    // Test_ArrayList::test_set();
+
+    Test_ArrayList::test_get_operator();
 
     return 0;
 }
