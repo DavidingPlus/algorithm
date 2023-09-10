4dedaf3a8c58a312bbc4024054c6b6db7fe3e7ff
DavidingPlus <2065666169@qq.com>
2023-09-10 20:34:30 +0800

    update 9.10

--- _test.cpp
+++ _test.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 using namespace std;
-#include <string>
-
-string Palindrome(string s, int i, int j) {
-    while (i >= 0 && j < s.size() && s[i] == s[j]) {
-        --i;
-        ++j;
-    }
-
-    return string(s.begin() + i + 1, s.begin() + j);
-}
 
 int main() {
-    cout << Palindrome("a", 0, 0) << endl;
-
     return 0;
 }
