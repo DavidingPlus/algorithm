93f4db81e32a5c718fb7a54505cfed22081793e6
DavidingPlus <2065666169@qq.com>
2023-09-20 22:41:55 +0800

    update 9.20

---
+++ LeetCode刷题/104.二叉树的最大深度.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (!root)
+            return 0;
+
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+// @lc code=end
---
+++ labuladong/二叉树/binary_tree.h
@@ -0,0 +1,46 @@
+#ifndef _Binary_Tree_H_
+#define _Binary_Tree_H_
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 二叉树的数据结构表示
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// 二叉树算法的两个核心问题：
+// 当前结点应该去做什么事情？
+// 什么时候去做这件事情？
+
+// 二叉树遍历框架
+void traverse(TreeNode *root) {
+    if (!root)
+        return;
+
+    // 我们用三个vector来存储不同的遍历结果
+    vector<int> pre_order;
+    vector<int> in_order;
+    vector<int> post_order;
+
+    // 访问该节点有三个时机，第一个是刚到这个节点，第二个是访问完左子树回到这个节点，第三个是访问完右子树最后回到这个结点
+    // 这三个节点就构成了三种不同的访问机制
+
+    // 前序遍历位置，代码写在这里
+    pre_order.push_back(root->val);
+
+    traverse(root->left);
+
+    // 中序遍历位置，代码写在这里
+    in_order.push_back(root->val);
+
+    traverse(root->right);
+
+    // 后序遍历位置，代码写在这里
+    post_order.push_back(root->val);
+}
+
+#endif
