2ec7c3a9dfcc5867b43d570e6a5925e6b42ce786
DavidingPlus <2065666169@qq.com>
2023-09-25 14:15:11 +0800

    update 9.25

---
+++ LeetCode刷题/331.验证二叉树的前序序列化.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=331 lang=cpp
+ *
+ * [331] 验证二叉树的前序序列化
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+#include <algorithm>
+#include <string>
+
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+    }
+};
+// @lc code=end
--- LeetCode刷题/513.找树左下角的值.cpp
+++ LeetCode刷题/513.找树左下角的值.cpp
@@ -32,37 +32,63 @@ using namespace std;
 
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode *root) {
-        if (!root)
-            return {};
+    int depth = 0;
+    pair<int, int> val_depth = {0, 0};
 
-        int ret;
-        queue<TreeNode *> q;
-        q.push(root);
+    // 我用递归遍历实现这个题
+    void traverse(TreeNode *root) {
+        if (nullptr == root)
+            return;
 
-        while (!q.empty()) {
-            double sum = 0;
-            int count = 0;
+        ++depth;
+        // 这样递归下去，每次遇到新的一层都是这一层最左边的
+        if (depth > val_depth.second)
+            val_depth = {root->val, depth};
 
-            int sz = q.size();
-            for (int i = 0; i < sz; ++i) {
-                auto cur = q.front();
+        traverse(root->left);
+        traverse(root->right);
 
-                // 遍历完毕自然就是最下层
-                if (0 == i)
-                    ret = cur->val;
-
-                // 将左右子树入队列
-                if (cur->left)
-                    q.push(cur->left);
-                if (cur->right)
-                    q.push(cur->right);
-
-                q.pop();
-            }
-        }
-
-        return ret;
+        --depth;
     }
+
+    int findBottomLeftValue(TreeNode *root) {
+        traverse(root);
+
+        return val_depth.first;
+    }
+
+    // 用层次遍历实现这个题
+    //  int findBottomLeftValue(TreeNode *root) {
+    //      if (!root)
+    //          return {};
+
+    //     int ret;
+    //     queue<TreeNode *> q;
+    //     q.push(root);
+
+    //     while (!q.empty()) {
+    //         double sum = 0;
+    //         int count = 0;
+
+    //         int sz = q.size();
+    //         for (int i = 0; i < sz; ++i) {
+    //             auto cur = q.front();
+
+    //             // 遍历完毕自然就是最下层
+    //             if (0 == i)
+    //                 ret = cur->val;
+
+    //             // 将左右子树入队列
+    //             if (cur->left)
+    //                 q.push(cur->left);
+    //             if (cur->right)
+    //                 q.push(cur->right);
+
+    //             q.pop();
+    //         }
+    //     }
+
+    //     return ret;
+    // }
 };
 // @lc code=end
