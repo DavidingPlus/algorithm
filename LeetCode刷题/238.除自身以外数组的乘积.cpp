5bbe50e05e31c3a920ac82ac887e7c3dc34a3972
DavidingPlus <2065666169@qq.com>
2023-10-07 13:43:00 +0800

    update 10.7

---
+++ LeetCode刷题/238.除自身以外数组的乘积.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=238 lang=cpp
+ *
+ * [238] 除自身以外数组的乘积
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    // 这个题不准用除法，那么只能用乘法，加上逆向思维，既然有前缀和自然也有对应的后缀和
+    vector<int> productExceptSelf(vector<int>& nums) {
+    }
+};
+// @lc code=end
