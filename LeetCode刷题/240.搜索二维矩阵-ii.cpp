f842aa999bd811b9604f7a7da17c891370d40d86
DavidingPlus <2065666169@qq.com>
2023-10-10 21:35:43 +0800

    update 10.10

---
+++ LeetCode刷题/240.搜索二维矩阵-ii.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=cpp
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    // 这个矩阵和前面一个题的性质不同，需要注意
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+
+    }
+};
+// @lc code=end
