a08c67f9dc59cc045447f4797f5f796c3367ca0a
刘治学 <2065666169@qq.com>
2023-11-26 15:22:02 +0800

    update 11.26

--- LeetCode刷题/297.二叉树的序列化与反序列化.cpp
+++ LeetCode刷题/297.二叉树的序列化与反序列化.cpp
@@ -4,6 +4,13 @@
  * [297] 二叉树的序列化与反序列化
  */
 
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -14,16 +21,73 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+#include <cstring>
+#include <iostream>
+#include <sstream>
+using namespace std;
+#include <list>
+#include <string>
 
-// TODO
 class Codec {
 public:
+    // 前序遍历方法
+    void traverse(TreeNode* root, string& str) {
+        // 元素之间用空格分开
+        if (nullptr == root) {
+            str += "#,";
+            return;
+        }
+
+        str += to_string(root->val) + ",";
+
+        traverse(root->left, str);
+        traverse(root->right, str);
+    }
+
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
+        string res;
+
+        traverse(root, res);
+
+        // 把末尾的','去掉
+        res.pop_back();
+
+        return res;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
+        // split
+        stringstream ss(data);
+        string item;
+        list<string> nodes;
+        while (getline(ss, item, ','))
+            if (!item.empty())
+                nodes.push_back(item);
+
+        return deserialize(nodes);
+    }
+
+    TreeNode* deserialize(list<string>& nodes) {
+        // 前序遍历只维护节点的信息是没办法还原成为二叉树的
+        // 但是现在我们维护有空指针的信息，每一个位置就是一个节点
+        // 我们按照前序的方式去遍历就可以了
+        if (nodes.empty())
+            return nullptr;
+
+        string str = nodes.front();
+        // 这里弹出头部不要用vector，他还要一步一步的挪，用list
+        nodes.pop_front();  // 这一步弹出的操作很关键，为了保证子问题拿到的数据和原问题自洽
+        if ("#" == str)
+            return nullptr;
+
+        TreeNode* root = new TreeNode(atoi(str.c_str()));
+        // 递归处理左右子树
+        root->left = deserialize(nodes);
+        root->right = deserialize(nodes);
+
+        return root;
     }
 };
 
---
+++ LeetCode刷题/652.寻找重复的子树.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=652 lang=cpp
+ *
+ * [652] 寻找重复的子树
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    vector<TreeNode *> findDuplicateSubtrees(TreeNode *root) {
+    }
+};
+// @lc code=end
