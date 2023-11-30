72a0542e84067c921636290fb4cf24c64cb8e036
DavidingPlus <2065666169@qq.com>
2023-11-30 13:03:10 +0800

    update 11.30:

---
+++ LeetCode刷题/496.下一个更大元素-i.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=496 lang=cpp
+ *
+ * [496] 下一个更大元素 I
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // 先处理nums2的，用单调栈去处理，然后再根据nums1去做映射就好了
+    }
+};
+// @lc code=end
