cf4bf1f73a8f76aff6f971d6c2a8d4712e2e5fcd
DavidingPlus <2065666169@qq.com>
2023-12-05 16:10:25 +0800

    update 12.5

--- LeetCode刷题/300.最长递增子序列.cpp
+++ LeetCode刷题/300.最长递增子序列.cpp
@@ -15,24 +15,6 @@ class Solution {
 public:
     // 这个子序列不一定是连续的！
     int lengthOfLIS(vector<int>& nums) {
-        // 定义dp数组为以i元素结尾的最长严格递增子序列的长度
-        vector<int> dp;
-        dp.resize(nums.size());
-        fill(dp.begin(), dp.end(), 1);  // 需要全部初始化为1，因为不满足条件就表明为1
-        int ret = dp[0];
-
-        for (int i = 1; i < nums.size(); ++i) {
-            // 现在的问题是如何已知dp[0]到dp[i-1]如何求出dp[i]
-            // 一个递增子序列除去最后一个肯定也是递增子序列
-            // 并且在本题中一定是以这个元素为结尾的递增子序列，否则就能找出更大的长度了
-            for (int j = 0; j < i; ++j)
-                if (nums[i] > nums[j])
-                    dp[i] = max(dp[i], 1 + dp[j]);
-
-            ret = max(ret, dp[i]);
-        }
-
-        return ret;
     }
 };
 // @lc code=end
--- LeetCode刷题/509.斐波那契数.cpp
+++ LeetCode刷题/509.斐波那契数.cpp
@@ -17,36 +17,33 @@ public:
     // 时间复杂度高的一个大问题：重复子问题的计算
     // 比如fib(19)+fib(18)：19又要拆分为18和17，18就计算了两次，而且还不止一个问题这么干了
     // int fib(int n) {
-    //     if (0 == n)
-    //         return 0;
-    //     if (1 == n)
-    //         return 1;
-
+    //     if (0 == n or 1 == n)
+    //         return n;
     //     return fib(n - 1) + fib(n - 2);
     // }
 
     // 通过备忘录进行了优化
-    // int Fib[N] = {0};
+    // int memo[N] = {0};
 
     // int fib(int n) {
-    //     if (0 == n)
-    //         return 0;
-    //     if (1 == n)
-    //         return 1;
+    //     // 先查备忘录
+    //     if (0 != memo[n])
+    //         return memo[n];
 
-    //     // 如果备忘录中存在直接用即可
-    //     if (0 != Fib[n])
-    //         return Fib[n];
-
-    //     Fib[n] = fib(n - 1) + fib(n - 2);
-    //     return Fib[n];
+    //     if (0 == n or 1 == n)
+    //         return n;
+    //     memo[n] = fib(n - 1) + fib(n - 2);
+    //     return memo[n];
     // }
 
     // 将递归变为迭代，通过线性dp求解
     // int fib(int n) {
-    //     int dp[N];  // 这个东西做备忘录
+    //     if (0 == n or 1 == n)
+    //         return n;
 
-    //     dp[0] = 0, dp[1] = 1;
+    //     int dp[N] = {0};
+    //     dp[1] = 1;
+
     //     for (int i = 2; i <= n; ++i)
     //         dp[i] = dp[i - 1] + dp[i - 2];
 
@@ -55,18 +52,17 @@ public:
 
     // 我们可以进一步优化空间复杂度为O(1)
     int fib(int n) {
-        if (0 == n || 1 == n)
+        if (0 == n or 1 == n)
             return n;
 
-        // 我们只用两个变量就可以记录n-1和n-2的数组值
-        int dp_i_2 = 0, dp_i_1 = 1;
+        // 我们用两个变量就能记录，不用开dp[]数组
+        int a = 0, b = 1;
         for (int i = 2; i <= n; ++i) {
-            int tmp = dp_i_2;
-
-            dp_i_2 = dp_i_1;
-            dp_i_1 = dp_i_1 + tmp;
+            int c = b;
+            b += a;
+            a = c;
         }
-        return dp_i_1;
+        return b;
     }
 };
 // @lc code=end
