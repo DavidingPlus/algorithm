21d7ec531dc875b13c1b55f469bebfbc393bd85a
DavidingPlus <2065666169@qq.com>
2023-12-08 14:46:20 +0800

    update 12.8

--- LeetCode刷题/300.最长递增子序列.cpp
+++ LeetCode刷题/300.最长递增子序列.cpp
@@ -15,6 +15,21 @@ class Solution {
 public:
     // 这个子序列不一定是连续的！
     int lengthOfLIS(vector<int>& nums) {
+        // 设置dp[i]的含义是以i下标结尾的最长递增子序列
+        // 从i-1推导i，就是看0到i-1的每个子序列加上i的元素之后能否满足递增，然后合起来进行比较即可，因为i结尾的子序列去掉最后一个仍然是递增子序列，并且由于是最大所以肯定是倒数第二个下标对应的最大的
+        int dp[N] = {0};
+        dp[0] = 1;
+        int res = 1;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i] = 1;  // 需要初始化，只有自己就是1
+            for (int j = 0; j < i; ++j)
+                if (nums[i] > nums[j])
+                    dp[i] = max(dp[i], 1 + dp[j]);
+
+            res = max(res, dp[i]);
+        }
+        return res;
     }
 };
 // @lc code=end
---
+++ LeetCode刷题/354.俄罗斯套娃信封问题.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=354 lang=cpp
+ *
+ * [354] 俄罗斯套娃信封问题
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+    }
+};
+// @lc code=end
