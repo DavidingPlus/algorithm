6be5948c6e5b14751f8e5c44a5d64ab9bbde074b
DavidingPlus <2065666169@qq.com>
2024-03-24 00:12:34 +0800

    update 3.24

--- 蓝桥杯真题/2022/Python-A组/02.cpp
+++ 蓝桥杯真题/2022/Python-A组/02.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 
+// greatest common divisor 最大公约数
+long long gcd(long long val1, long long val2)
+{
+    // 辗转相除法
+    // a 被除数 b 除数
+    auto a = std::max(val1, val2), b = std::min(val1, val2);
+    while (0 != a % b)
+    {
+        auto newB = a % b;
+        a = b;
+        b = newB;
+    }
+    return b;
+}
+
+// least common multiple 最小公倍数
+long long lcm(long long val1, long long val2)
+{
+    return val1 / gcd(val1, val2) * val2; // 乘法在除法之前进行，可以避免溢出
+}
+
 int main()
 {
+    int mod[50] = {0, 0, 1, 2, 1, 4, 5, 4, 1, 2, 9, 0, 5, 10, 11, 14, 9, 0, 11, 18, 9, 11, 11, 15, 17, 9, 23, 20, 25, 16, 29, 27, 25, 11, 17, 4, 29, 22, 37, 23, 9, 1, 11, 11, 33, 29, 15, 5, 41, 46};
 
-    int i = 87613307;
-    while (1)
+    // 从第一个数开始，比如只考虑第一个数，满足条件的最小值是 3 ，后续要满足就加上 2 ，现在我加入下一个数 3 ，先找到在步长 2 的基础上能满足这二者的最小的数是 5，想要继续满足，需要更新步长为 2 和 3 的最小公倍数 6 才行
+    // 可能会思考到这种问题，就是前面已经加入的数字得出的结果已经能够满足后面的数字，但是要记住，上面数学归纳法的前提是满足前面 n 个数的最小值，所以即使满足 n+1 ，整体也是不满足的，因此不必担心，这也是一种数学归纳法和动态规划的思想
+    long long ans = 3, step = 2; // 初始状态
+    for (int i = 3; i < 50; ++i)
     {
-        int num = i * (11 * 17);
-        if (1 == num % 2 and 2 == num % 3 and 1 == num % 4 and 5 == num % 6 and 4 == num % 7 and 1 == num % 8 and 2 == num % 9 and 9 == num % 10 and 5 == num % 12 and 10 == num % 13 and 11 == num % 14 and 14 == num % 15 and 9 == num % 16 and 11 == num % 18 and 18 == num % 19 and 9 == num % 20 and 11 == num % 21 and 15 == num % 23 and 17 == num % 24 and 9 == num % 25 and 23 == num % 26 and 20 == num % 27 and 25 == num % 28 and 16 == num % 29 and 29 == num % 30 and 27 == num % 31 and 25 == num % 32 and 11 == num % 33 and 17 == num % 34 and 4 == num % 35 and 29 == num % 36 and 22 == num % 37)
-        {
-            print(num);
-            break;
-        }
-        ++i;
+        // 加入新的数，首先在原步长上进行判断
+        while (mod[i] != ans % i)
+            ans += step;
+        // 更新步长
+        step = lcm(step, i);
     }
+
+    std::cout << ans << '\n'; // 2022040920220409
 
     return 0;
 }
--- 蓝桥杯真题/2022/Python-A组/02.py
+++ 蓝桥杯真题/2022/Python-A组/02.py
@@ -1,13 +1,29 @@
 # https://www.lanqiao.cn/problems/2131/learning/?page=1&first_category_id=1&name=%E5%AF%BB%E6%89%BE
 
-# 根据图，这个数必然是 11 和 17 的倍数
+# 更详细的注释见 .cpp
 
-# 下面的条件不可能一次罗列完，所以每次的 i 下限是会改变的，为了节省时间
-# 好吧这个程序我为了节省时间用 cpp 跑的
-i = 87613307
-while True:
-    num = i * (11*17)
-    if 1 == num % 2 and 2 == num % 3 and 1 == num % 4 and 5 == num % 6 and 4 == num % 7 and 1 == num % 8 and 2 == num % 9 and 9 == num % 10 and 5 == num % 12 and 10 == num % 13 and 11 == num % 14 and 14 == num % 15 and 9 == num % 16 and 11 == num % 18 and 18 == num % 19 and 9 == num % 20 and 11 == num % 21 and 15 == num % 23 and 17 == num % 24 and 9 == num % 25 and 23 == num % 26 and 20 == num % 27 and 25 == num % 28 and 16 == num % 29 and 29 == num % 30 and 27 == num % 31 and 25 == num % 32 and 11 == num % 33 and 17 == num % 34 and 4 == num % 35 and 29 == num % 36 and 22 == num % 37:
-        print(num)
-        break
-    i += 1
+# greatest common divisor 最大公约数
+def gcd(val1, val2):
+    # 辗转相除法
+    # a 被除数 b 除数
+    a, b = max(val1, val2), min(val1, val2)
+    while a % b != 0:
+        a, b = b, a % b
+    return b
+
+# least common multiple 最小公倍数
+def lcm(val1, val2):
+    return val1 // gcd(val1, val2) * val2
+
+mod = [0, 0, 1, 2, 1, 4, 5, 4, 1, 2, 9, 0, 5, 10, 11, 14, 9, 0, 11, 18, 9, 11, 11, 15, 17, 9, 23, 20, 25, 16, 29, 27, 25, 11, 17, 4, 29, 22, 37, 23, 9, 1, 11, 11, 33, 29, 15, 5, 41, 46]
+
+# 初始状态
+ans, step = 3, 2
+for i in range(3, 50):
+    # 加入新的数，首先在原步长上进行判断
+    while mod[i] != ans % i:
+        ans += step
+    # 更新步长
+    step = lcm(step, i)
+
+print(ans)  # 输出结果
