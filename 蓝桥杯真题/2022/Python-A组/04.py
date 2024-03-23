09bd964c6f5e46d2382a3c6d0c20d027c430adfb
DavidingPlus <2065666169@qq.com>
2024-03-24 00:13:15 +0800

    update 3.24

--- 蓝桥杯真题/2022/Python-A组/04.py
+++ 蓝桥杯真题/2022/Python-A组/04.py
@@ -1,1 +1,7 @@
 # https://www.lanqiao.cn/problems/2154/learning/?page=1&first_category_id=1&name=%E7%9F%A9%E5%BD%A2%E6%8B%BC%E6%8E%A5
+
+# 即使是最差的情况，也能拼成八边形
+n = int(input())
+for i in range(n):
+    a1, b1, a2, b2, a3, b3 = map(int, input().split(' '))
+    # TODO
