83d02fc94f28b3a1888807fb6a47bb7d275eae63
DavidingPlus <2065666169@qq.com>
2024-03-26 21:01:35 +0800

    update 3.26

--- 蓝桥杯真题/2021/Python组/07.py
+++ 蓝桥杯真题/2021/Python组/07.py
@@ -1,28 +1,31 @@
 # https://www.lanqiao.cn/problems/1457/learning/?page=1&first_category_id=1&name=%E6%9D%A8%E8%BE%89%E4%B8%89%E8%A7%92%E5%BD%A2
 
-# 初始化一个杨辉三角的雏形，答案就在构造杨辉三角的过程中就可以得到
-n = int(input())
-# 直接存储上一层即可，计算出下一层在赋值
-matrix = [1, 1]
+from math import comb, factorial
 
-# 开始生成杨辉三角
+# math 里面没有计算排列的公式，根据这个组合和阶乘的公式自己写一个
+# 要保证 a 等于 b ，否则会报错
+# def Comb(n, m):
+#     return factorial(n) // factorial(m) // factorial(n - m)
+# def perm(a, b):
+#     return Comb(a, b) * factorial(b)
+# print(comb(5, 2))  # 10
+# print(perm(5, 2))  # 20
+
+# 从第三行开始，如果最大值和最小值满足范围，则查询（不包括 1）
+# TODO 超时，4/10
+n = int(input())
 ans = 1
-# n 为 1 的情况需要特判，不进入下面的计算
-# TODO 算法超时，4/10
 if 1 != n:
+    i = 2  # 注意 2 是下标不是行号
     while True:
-        flag = False
-        tmp = [1]
-        i = len(matrix)
-        for j in range(1, i):
-            tmp.append(matrix[j-1]+matrix[j])
-            # 第一次出现，计算下标
-            if (n == tmp[j]):
-                flag = True
-                ans = i * (i+1)//2 + j + 1
+        if comb(i, 1) <= n and comb(i, i // 2) >= n:
+            flag = False
+            for j in range(1, 1 + i // 2):
+                if n == comb(i, j):
+                    ans = i * (i + 1) // 2 + j + 1
+                    flag = True
+                    break
+            if True == flag:
                 break
-        if True == flag:
-            break
-        tmp.append(1)  # 不要忘了末尾补上 1
-        matrix = tmp
+        i += 1
 print(ans)
