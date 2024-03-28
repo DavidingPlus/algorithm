d158eff8b95291c38ee70b1b2200d04440c4338d
DavidingPlus <2065666169@qq.com>
2024-03-28 16:14:40 +0800

    update 3.28

---
+++ LeetCode刷题/1049.最后一块石头的重量-ii.py
@@ -0,0 +1,10 @@
+#
+# @lc app=leetcode.cn id=1049 lang=python3
+#
+# [1049] 最后一块石头的重量 II
+#
+
+# @lc code=start
+class Solution:
+    def lastStoneWeightII(self, stones: list[int]) -> int:
+# @lc code=end
