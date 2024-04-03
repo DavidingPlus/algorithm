1e2ede13840a96d3bd3e5e94c5721a77f9b52079
DavidingPlus <2065666169@qq.com>
2024-04-03 13:15:35 +0800

    update 4.3

--- LeetCode刷题/463.岛屿的周长.py
+++ LeetCode刷题/463.岛屿的周长.py
@@ -12,17 +12,20 @@ class Solution:
     def islandPerimeter(self, grid) -> int:
         m, n = len(grid), len(grid[0])
 
-        # 显然是广度优先，一个块往外扩散一个，周长加 2
-        # TODO：初步逻辑是这样，但是有问题
+        # 显然是广度优先，一个块往外扩散一个，周长变化一定的值
         # 找到第一个非 0 的区域
-        x, y = [0, 0]
+        x, y = [-1, -1]
         for i in range(m):
+            flag = False
             for j in range(n):
                 if 1 == grid[i][j]:
+                    flag = True
                     x, y = [i, j]
                     break
+            if flag:
+                break
 
-        if 0 == x and 0 == y:
+        if -1 == x and -1 == y:
             return 0
 
         # BFS
@@ -43,7 +46,14 @@ class Solution:
                     if newX >= 0 and newX < m and newY >= 0 and newY < n and 1 == grid[newX][newY] and False == isVisited[newX][newY]:
                         q.append([newX, newY])
                         isVisited[newX][newY] = True
-                        ans += 2
+                        # 周长变化，加上新方块周围 not isVisited 的个数，减去 isVisited 的个数；如果新方块已经超出边界，那么应该加 1
+                        for dir2 in dirs:
+                            newX2, newY2 = newX+dir2[0], newY+dir2[1]
+                            if newX2 < 0 or newX2 >= m or newY2 < 0 or newY2 >= n:
+                                ans += 1
+                            else:
+                                ans += 1 if False == isVisited[newX2][newY2] else -1
+
         return ans
 
 # @lc code=end
--- 蓝桥杯复习/template/3-快速幂取余.py
+++ 蓝桥杯复习/template/3-快速幂取余.py
@@ -1,15 +1,30 @@
 # 递归快速幂取余算法（求一个数的次方，也就是个大数对某个数的取模）
-def quickPowModRecursion(a, n, mod):
+def quickPowModRecursion(base: int, power: int, mod: int) -> int:
     # 递归出口
-    if 0 == n:
+    if 0 == power:
         return 1
     # 下面的推导会用到一个公式，两个数乘积除以某个数的余数，等于这两个数分别取余再相乘再取余；因此就可以递归了
-    elif n % 2 == 1:
-        return quickPowModRecursion(a, n - 1, mod) * a % mod
+    elif power & 1:
+        return (base * quickPowModRecursion(base, power - 1, mod)) % mod
     else:
-        tmp = quickPowModRecursion(a, n / 2, mod) % mod
-        return tmp * tmp % mod
+        tmp = quickPowModRecursion(base, power // 2, mod) % mod
+        return (tmp * tmp) % mod
+
+
+# 非递归快速幂取余算法（利用位运算，迭代）
+# 以 2 ** 5 次方为例，5 的二进制为 101，并且发现 2 ** 5 == (2 ** (2 ** 2)) * (2 ** (2 ** 0))
+# 而 2 ** n = (2 ** (n-1)) ** 2，因此状态转移方程就出来了
+# 这里的 a, n ,mod 都是基本数据类型，pass by value，不会改变形参，可以直接修改
+def quickPowModNoRecursion(base: int, power: int, mod: int) -> int:
+    res = 1
+    while power > 0:
+        if power & 1:  # 得到末尾结果是 1 还是 0 ，以此判断该位是否需要乘上去
+            res = (res * base) % mod
+        base = (base * base) % mod  # base 每次平方，到达当前位的时候才是正确的底数
+        power >>= 1  # 指数后移，得到每一位的 01 值
+    return res
 
 
 a, n, mod = 20190324, 7835954031267158017, 1001733993063167141
 print(quickPowModRecursion(a, n, mod))  # 579706994112328949
+print(quickPowModNoRecursion(a, n, mod))  # 579706994112328949
