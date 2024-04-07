96beebdb2509bf46205b8f56b5f29af19b0bd4dc
DavidingPlus <2065666169@qq.com>
2024-04-07 18:11:07 +0800

    update 4.7

---
+++ 蓝桥杯复习/2024/刷题/4.7/02.py
@@ -0,0 +1,7 @@
+# https://www.lanqiao.cn/problems/2410/learning/?page=1&first_category_id=1&second_category_id=3&tags=2023
+
+file = open("./02.txt", encoding="utf-8", mode='r')
+grid = file.readlines()
+file.close()
+
+print(grid)
