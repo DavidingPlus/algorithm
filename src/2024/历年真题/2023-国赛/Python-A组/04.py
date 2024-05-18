66b81c25118c7a94dfbfcbe2c1c9edce03b97c34
DavidingPlus <2065666169@qq.com>
2024-05-18 23:27:18 +0800

    update 5.18

---
+++ src/2024/历年真题/2023-国赛/Python-A组/04.py
@@ -0,0 +1,1 @@
+# https://www.lanqiao.cn/problems/17116/learning/?page=1&first_category_id=1&name=2023
---
+++ src/2024/历年真题/2023-国赛/Python-A组/05.py
@@ -0,0 +1,1 @@
+# https://www.lanqiao.cn/problems/17117/learning/?page=1&first_category_id=1&name=%E7%81%AB%E8%BD%A6%E8%BF%90%E8%BE%93
