9b190c52afd5a6db3b9e30a5fcf45f8225d9e0f7
DavidingPlus <2065666169@qq.com>
2024-05-23 18:46:04 +0800

    update 5.23

--- src/2024/刷题/5.23/08.py
+++ src/2024/刷题/5.23/08.py
@@ -1,5 +1,22 @@
 # 单点修改，区间查询
 # https://www.lanqiao.cn/problems/2340/learning/
 
+from math import sqrt
+
 # 通过分块的思想进行优化
-# TODO
+N = int(input())
+nums = list(map(int, input().split()))
+
+# 计算出分块出来的区间的长度，最佳的值是根号 n ，这里为了防止为 0 ，加 1
+each = int(sqrt(N))+1
+sums = [0 for _ in range(N//each if 0 == N % each else 1+N//each)]
+for i in range(N):
+    sums[i//each] += nums[i]
+
+m = int(input())
+for _ in range(m):
+    l = list(map(int, input().split()))
+    if 1 == l[0]:
+        sums[l[1]//each] += l[2]
+    else:
+        # TODO
