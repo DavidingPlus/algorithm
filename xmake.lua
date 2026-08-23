f6d56eec1eb2bcb691e30f4843c62a9248abe586
DavidingPlus <davidingplus@qq.com>
2026-08-23 17:15:59 +0800

    Update root XMake project metadata

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,7 @@
-set_project("algorithm")
-set_version("0.1.0")
+add_rules("mode.debug", "mode.release")
 
--- XMake has a cross-platform local compilation cache enabled by default.
--- Keep both CMake and XMake builds under the same project root.
+set_project("algorithm")
+set_version("1.0.0")
 
 includes("leetcode/cpp")
 includes("hello-algo")
