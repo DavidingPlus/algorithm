cdb34a134179a15dd021efc7c93088a792ce6d6a
DavidingPlus <davidingplus@qq.com>
2026-08-24 20:26:49 +0800

    working on leetcode 2026/08/24

--- leetcode/cpp-template/leetcode/editor/cn/3.无重复字符的最长子串.cpp
+++ leetcode/cpp-template/leetcode/editor/cn/3.无重复字符的最长子串.cpp
@@ -10,32 +10,42 @@
 
 // @lc code=start
 
+class Solution
+{
 
-const int N = 5 * 1e4 + 10;
-
-class Solution {
 public:
-    int lengthOfLongestSubstring(std::string s) {
-        if (s.empty())
-            return 0;
 
-        // 滑动窗口在动态规划中的应用
-        // dp[i]代表以i字符结尾的最长不重复子串
-        int res = 1, dp[N] = {0};
+    int lengthOfLongestSubstring(std::string s)
+    {
+        if (s.empty()) return 0;
+
+        // 维护一个滑动窗口（左闭右开），初始 left = 0, right = 0。当 right 往右移动的过程中，如果出现重复字符，代表 right 往右的子串全部都不满足，此时应该移动 left，使得重复字符移出窗口。然后进行下一轮的循环。
+        // 其实这等价于一个一维线性 DP 问题，使用 dp[i] 代表以 i 结尾的不含有重复字符的最长子串的长度。当下一个字符进来的时候，根据是否重复的逻辑去更新 dp[i + 1] 即可。
+
+        std::string_view sv{s};
+        int res = 1;
+        // 滑动窗口在动态规划中的应用，dp[i] 代表以 i 字符结尾的最长不重复子串的长度。
+        std::vector<int> dp(s.size());
         dp[0] = 1;
 
-        for (int i = 1; i < s.size(); ++i) {
-            // 上一个子串肯定是不重复的，所以find和rfind没区别
-            std::size_t pos = std::string(s.begin() + i - dp[i - 1], s.begin() + i).find(s[i]);
+        for (int i = 1; i < s.size(); ++i)
+        {
+            // 在当前不含有重复字符的子串中查找新进来的字符，因为 dp 对应的子串我们从语义上保证不重复，因此查找结果只有一个唯一值。
+            // 注意这个 pos 是相对子串的下标，不是整串 s。
+            auto pos = sv.substr(i - dp[i - 1], dp[i - 1]).find(s[i]);
 
-            // 如果找不到直接更新
-            if (std::string::npos == pos)
+            if (std::string_view::npos == pos)
+            {
                 dp[i] = 1 + dp[i - 1];
+            }
             else
-                dp[i] = i - (pos + i - dp[i - 1]);
+            {
+                dp[i] = i - (i - dp[i - 1] + pos);
+            }
 
             res = std::max(res, dp[i]);
         }
+
 
         return res;
     }
@@ -47,4 +57,6 @@ int main()
 {
     Solution solution;
     // your test code here
+
+    std::cout << solution.lengthOfLongestSubstring("abcabcbb") << std::endl;
 }
--- leetcode/cpp-template/leetcode/editor/cn/543.二叉树的直径.cpp
+++ leetcode/cpp-template/leetcode/editor/cn/543.二叉树的直径.cpp
@@ -4,7 +4,6 @@
  * [543] 二叉树的直径
  */
 
-
 #include "../common/globalmacros.h"
 #include "../common/ListNode.cpp"
 #include "../common/TreeNode.cpp"
@@ -23,27 +22,29 @@
  * };
  */
 
-class Solution {
+class Solution
+{
+
 public:
-    int diameterOfBinaryTree(TreeNode *root) {
-        // 过每个节点的最长直径就是左子树的高度加上右子树的高度
-        // 遍历所有的节点获得最大值即可
+
+    int res = 0;
+
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        // 对每个结点而言，过该结点的最长直径就是左子树的高度加上右子树的高度，因此只需遍历所有的节点获得最大值即可。
         depth(root);
         return res;
     }
 
-    int res = 0;
+    int depth(TreeNode *root)
+    {
+        if (!root) return 0;
 
-    int depth(TreeNode *root) {
-        if (nullptr == root)
-            return 0;
+        // 如果我这里改为后序遍历，我就能够在下面获得函数的返回值，进而在一次遍历的时候就能够获得过该节点的最大直径。这个思路算是遍历加上划分为子问题合起来吧。
+        int leftDepth = depth(root->left), rightDepth = depth(root->right);
 
-        int leftDepth = depth(root->left);
-        int rightDepth = depth(root->right);
-
-        // 如果我这里改为后序遍历，我就能够在下面获得函数的返回值，进而在一次遍历的时候就能够获得过该节点的最大直径
-        // 这个思路算是遍历加上划分为子问题合起来吧...
         res = std::max(res, leftDepth + rightDepth);
+
 
         return 1 + std::max(leftDepth, rightDepth);
     }
@@ -54,5 +55,11 @@ public:
 int main()
 {
     Solution solution;
-    // your test code here
+
+    std::vector<std::optional<int>> values{4, -7, -3, std::nullopt, std::nullopt, -9, -3, 9, -7, -4, std::nullopt, 6, std::nullopt, -6, -6, std::nullopt, std::nullopt, 0, 6, 5, std::nullopt, 9, std::nullopt, std::nullopt, -1, -4, std::nullopt, std::nullopt, std::nullopt, -2};
+    TreeNode *root = TreeNode::createRoot(values);
+
+    std::cout << solution.diameterOfBinaryTree(root) << std::endl; // 8
+
+    TreeNode::freeTree(root);
 }
