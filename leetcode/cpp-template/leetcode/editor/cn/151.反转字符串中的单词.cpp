b8c84cee73f5d104a6acf99e19408a686036e34f
DavidingPlus <davidingplus@qq.com>
2026-08-27 19:50:03 +0800

    working on leetcode 2026/08/27 evening

--- leetcode/cpp-template/leetcode/editor/cn/151.反转字符串中的单词.cpp
+++ leetcode/cpp-template/leetcode/editor/cn/151.反转字符串中的单词.cpp
@@ -18,28 +18,28 @@ public:
     const std::string SEP = " ";
 
 
-    std::string reverseWords(std::string s)
-    {
-        std::vector<std::string> words;
-        std::stringstream ss(s);
-        std::string token;
+    // std::string reverseWords(std::string s)
+    // {
+    //     std::vector<std::string> words;
+    //     std::stringstream ss(s);
+    //     std::string token;
 
-        // getline 按 SEP[0] 切分字符串；如果分隔符连续出现（比如 "   "），或者出现在开头，就可能读取到空 token。token.empty() 只负责阻止它加入 words。
-        while (std::getline(ss, token, SEP[0]))
-        {
-            if (!token.empty()) words.emplace_back(token);
-        }
+    //     // getline 按 SEP[0] 切分字符串；如果分隔符连续出现（比如 "   "），或者出现在开头，就可能读取到空 token。token.empty() 只负责阻止它加入 words。
+    //     while (std::getline(ss, token, SEP[0]))
+    //     {
+    //         if (!token.empty()) words.emplace_back(token);
+    //     }
 
-        std::string res;
-        for (int i = words.size() - 1; i >= 0; --i)
-        {
-            res += words[i];
-            if (0 != i) res += SEP;
-        }
+    //     std::string res;
+    //     for (int i = words.size() - 1; i >= 0; --i)
+    //     {
+    //         res += words[i];
+    //         if (0 != i) res += SEP;
+    //     }
 
 
-        return res;
-    }
+    //     return res;
+    // }
 
     // 旧版代码。
     // std::string reverseWords(std::string s)
@@ -85,14 +85,80 @@ public:
     //     return res;
     // }
 
-    // TODO 如何在原地修改的情况下，即空间复杂度为 O(1) 下完成本题？
-    // 核心是三步：
-    // 1. 原地删除多余空格，保证单词之间只有一个空格。
-    // 2. 原地反转整个字符串。
-    // 3. 再原地反转每个单词。
-    // std::string reverseWords(std::string s)
-    // {
-    // }
+    // 如何在原地修改的情况下，即空间复杂度为 O(1) 下完成本题？
+    std::string reverseWords(std::string s)
+    {
+        // 1. 原地删除多余空格，保证单词之间只有一个空格。
+
+        // 使用快慢指针的思路，快指针读到的有效字符往慢指针的位置填充。
+        // fast 负责读取原字符串，slow 负责写入压缩后的字符串。
+        // slow 不会超过 fast，因此写入不会覆盖尚未读取的字符。
+        // 例如："  a good   example  " 会被压缩成 "a good example"。
+        int fast = 0, slow = 0, newSize = 0;
+
+        for (; fast < s.size(); ++fast)
+        {
+            // 正常填充字符。
+            if (SEP[0] != s[fast])
+            {
+                s[slow++] = s[fast];
+                // slow 指向下一个待写入位置，也就是当前有效内容的长度。
+                newSize = slow;
+            }
+            // 遇到分割符。
+            else
+            {
+                // 只有已经写入过内容，且上一个写入字符不是空格时，才补充一个空格。
+                // 1. 0 != slow 代表不是头部的分隔符。
+                // 2. 如果上一个位置（slow - 1）已经是分隔符了，不能继续填充重复字符。
+                if (0 != slow && SEP[0] != s[slow - 1])
+                {
+                    // 填充分割符的情况下，newSize 需要将分隔符排除，因为可能后续全是分隔符，不能将尾部的分割符记录下来。如果后面是正常字符，newSize 会重新在前面更新。
+                    newSize = slow;
+                    s[slow++] = SEP[0];
+                }
+            }
+        }
+
+        // slow 后面的内容已经无效，通过 resize 删除。newSize 保证末尾空格不会被保留。
+        s.resize(newSize);
+
+        // 空字符串不能直接使用 s.size() - 1，因为 size() 是无符号类型，为空时会发生下溢。
+        if (s.empty()) return s;
+
+        // 2. 原地反转整个字符串。
+        // 例如："a good example" -> "elpmaxe doog a"。
+        reverseStrFromRange(s, 0, s.size() - 1);
+
+        // 3. 再原地反转每个单词。
+        // 整体反转后，每个单词内部的字符顺序也反了，因此再按照空格找到每个单词并单独反转。
+        // 例如："elpmaxe doog a" -> "example good a"。
+        int start = 0, offset = 0;
+        while (true)
+        {
+            offset = s.find(SEP[0], start);
+            // 找不到分隔符了，说明是最后的一段。
+            if (std::string::npos == offset)
+            {
+                reverseStrFromRange(s, start, s.size() - 1);
+                break;
+            }
+            // 正常翻转。
+            else
+            {
+                reverseStrFromRange(s, start, offset - 1);
+                start = 1 + offset;
+            }
+        }
+
+
+        return s;
+    }
+
+    void reverseStrFromRange(std::string &s, int left, int right)
+    {
+        while (left < right) std::swap(s[left++], s[right--]);
+    }
 };
 
 // @lc code=end
--- leetcode/cpp-template/leetcode/editor/cn/92.反转链表 II.cpp
+++ leetcode/cpp-template/leetcode/editor/cn/92.反转链表 II.cpp
@@ -27,43 +27,79 @@ class Solution
 
 public:
 
-    // 迭代解法。
-    ListNode *reverseBetween(ListNode *head, int left, int right)
-    {
-        ListNode *dummyHead = new ListNode(-1);
-        ListNode *p = head, *pDummy = dummyHead;
-        int i = 1;
-
-        // 先处理前面的部分。直接把 dummyHead->next 接到 head 即可。
-        dummyHead->next = head;
-        // 然后通过遍历拿到需要反转链表的首元素指针。
-        for (; i < left; ++i) p = p->next, pDummy = pDummy->next;
-
-        ListNode *reverseTail = nullptr;
-
-        // 处理中间反转链表的部分。
-        for (; i <= right; ++i)
-        {
-            ListNode *node = new ListNode(p->val);
-
-            // 第一个节点最终会成为反转链表的尾节点，提前记录下来防止后面重新遍历。
-            if (!reverseTail) reverseTail = node;
-
-            p = p->next;
-            node->next = pDummy->next, pDummy->next = node;
-        }
-
-        // 处理后面的部分。把反转链表的末尾和后面的部分连上即可。
-        reverseTail->next = p;
-
-
-        return dummyHead->next;
-    }
-
-    // TODO 递归解法。
+    // 解法 1：迭代。
     // ListNode *reverseBetween(ListNode *head, int left, int right)
     // {
+    //     ListNode *dummyHead = new ListNode(-1);
+    //     ListNode *p = head, *pDummy = dummyHead;
+    //     int i = 1;
+
+    //     // 先处理前面的部分。直接把 dummyHead->next 接到 head 即可。
+    //     dummyHead->next = head;
+    //     // 然后通过遍历拿到需要反转链表的首元素指针。
+    //     for (; i < left; ++i) p = p->next, pDummy = pDummy->next;
+
+    //     ListNode *reverseTail = nullptr;
+
+    //     // 处理中间反转链表的部分。
+    //     for (; i <= right; ++i)
+    //     {
+    //         ListNode *node = new ListNode(p->val);
+
+    //         // 第一个节点最终会成为反转链表的尾节点，提前记录下来防止后面重新遍历。
+    //         if (!reverseTail) reverseTail = node;
+
+    //         p = p->next;
+    //         node->next = pDummy->next, pDummy->next = node;
+    //     }
+
+    //     // 处理后面的部分。把反转链表的末尾和后面的部分连上即可。
+    //     reverseTail->next = p;
+
+
+    //     return dummyHead->next;
     // }
+
+    // 解法 2：递归。
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        if (!head) return nullptr;
+        if (left == right) return head;
+
+        // 1 == left，说明当前节点就是反转区间的起点。
+        if (1 == left)
+        {
+            return reverseN(head, right);
+        }
+        // 否则递归寻找。
+        else
+        {
+            head->next = reverseBetween(head->next, left - 1, right - 1);
+            return head;
+        }
+    }
+
+    // 存储从 head 开始的第 n + 1 个结点，即不用翻转的第一个结点。
+    ListNode *successor = nullptr;
+
+    // 反转从 head 开始的后 n 个结点。
+    ListNode *reverseN(ListNode *head, int n)
+    {
+        // 剩余一个结点需要反转的时候，记录后继结点，并直接返回原链表。
+        // 因为下面是后序操作，因此轮到赋值的时候 successor 已经被赋值好了。
+        if (1 == n)
+        {
+            successor = head->next;
+            return head;
+        }
+
+        ListNode *newHead = reverseN(head->next, n - 1);
+        head->next->next = head;
+        head->next = successor;
+
+
+        return newHead;
+    }
 };
 // @lc code=end
 
